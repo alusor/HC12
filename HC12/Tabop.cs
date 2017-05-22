c7549106f04e179320d354319b9b53ebea3d993c
eduardoz <eddvelez@outlook.com>
2017-05-21 19:39:21 -0500

    archivo de estructura para el tabop

---
+++ HC12/Tabop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HC12
+{
+    class Tabop
+    {
+        public string instruccion { get; set; }
+        public string operando { get; set; }
+        public string direccionamiento { get; set; }
+        public string codigoMaquina { get; set; }
+        public string bytesPorCalcular { get; set; }
+        public string totalDeBytes { get; set; }
+    }
+}
